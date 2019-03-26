efab28167f4be919c250b0076fa19cd03bf2460a
Chris Cinelli <ccinelli@ebay.com>
2019-03-25 23:01:08 -0700

    change default port and start only SSL if the SSL port is not there

--- src/server.js
+++ src/server.js
@@ -10,13 +10,19 @@ const app = express() // You can also use Express
 		sapper.middleware()
 	);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
 import spdy from 'spdy';
-const sslPort = process.env.SSL_PORT || 8443;
-const fs = require('fs');
-const sslOpt = {
-    key:  fs.readFileSync(`${__dirname}/../../certificates/server.key`),
-    cert: fs.readFileSync(`${__dirname}/../../certificates/server.crt`),
-};
-spdy.createServer(sslOpt, app).listen(sslPort, () => console.log(`Listening HTTP2 on port ${sslPort}`));
+if (process.env.SSL_PORT !== 0) {
+	const sslPort = process.env.SSL_PORT || 8443;
+	const fs = require('fs');
+	try {
+		const sslOpt = {
+			key:  fs.readFileSync(`${__dirname}/../../certificates/server.key`),
+			cert: fs.readFileSync(`${__dirname}/../../certificates/server.crt`),
+		};
+		spdy.createServer(sslOpt, app).listen(sslPort, () => console.log(`Listening HTTP2 on port ${sslPort}`));
+	} catch (e) {
+		console.log(e.stack || e);
+	}
+}

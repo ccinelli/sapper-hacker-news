a8828f8704648a198ff93d2b24f30027fa22a42d
Chris Cinelli <ccinelli@ebay.com>
2019-03-25 17:15:54 -0700

    Adding support for ssl certificates

--- src/server.js
+++ src/server.js
@@ -3,10 +3,20 @@ import compression from 'compression';
 import serve from 'serve-static';
 import * as sapper from '../__sapper__/server.js';
 
-express() // You can also use Express
+const app = express() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		serve('static'),
 		sapper.middleware()
-	)
-	.listen(process.env.PORT);
+	);
+
+app.listen(process.env.PORT || 3000);
+
+import spdy from 'spdy';
+const sslPort = process.env.SSL_PORT || 8443;
+const fs = require('fs');
+const sslOpt = {
+    key:  fs.readFileSync(`${__dirname}/../../certificates/server.key`),
+    cert: fs.readFileSync(`${__dirname}/../../certificates/server.crt`),
+};
+spdy.createServer(sslOpt, app).listen(sslPort, () => console.log(`Listening HTTP2 on port ${sslPort}`));
